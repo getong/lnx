fbe80d34c90394c7e82072fbcad253094c9b08c3
chillfish8 <hburt2003@gmail.com>
2024-11-15 16:37:19 +0000

    Add core changes for v0.10.0 lightning

---
+++ lnx/src/commands.rs
@@ -0,0 +1,58 @@
+use std::net::SocketAddr;
+use std::path::PathBuf;
+use clap::Subcommand;
+use tracing::info;
+use url::Url;
+
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Run the lnx SQL server
+    Run {
+        #[arg(short, long, env = "LNX_LISTEN_ADDRESS", default_value = "127.0.0.1:4202")]
+        /// The server bind address. {n}
+        /// {n}
+        /// * For local development this should be `127.0.0.1`. {n}
+        /// * For external use this should be `0.0.0.0`.
+        listen_address: SocketAddr,
+        #[arg(long, env = "LNX_DATA_PATH", default_value = "/var/lib/lnx/data/")]
+        /// The path where lnx should write all data to.
+        ///
+        /// NOTE: This does not include temporary files.
+        data_path: PathBuf,
+    },
+    /// Run the lnx SQL shell
+    Shell {
+        #[arg(long, env = "LNX_HOST")]
+        /// The host lnx server to connect to.
+        ///
+        /// This can be either `http` or `https` URL containing any
+        /// necessary connection information.
+        host: Url,
+    }
+}
+
+impl Commands {
+    /// Triggers any additional startup messages which are aware
+    /// of the provided subcommand.
+    pub fn display_startup_message(&self) {
+        match self {
+            Commands::Run { listen_address, data_path } => {
+                info!(listen_address = %listen_address, data_path = %data_path.display(), "Starting the lnx SQL server");  
+                
+                if listen_address.ip().is_loopback() {
+                    let extra = if listen_address.port() == 4202 {
+                        "".to_string()
+                    } else {
+                        format!("?p={}", listen_address.port())
+                    };
+                    
+                    info!("You can access the local UI @ https://my.lnx.rs/dashboard{extra}")
+                }
+            },
+            Commands::Shell { host } => {
+                info!(host = %host, "Connecting to the lnx SQL server");
+            },
+        }
+    }
+}
---
+++ lnx/src/main.rs
@@ -0,0 +1,55 @@
+mod logging;
+mod commands;
+
+use anyhow::Context;
+use clap::{Parser};
+use tracing::info;
+use crate::logging::init_logging;
+
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+/// lnx, why settle for less when you can have more?
+///
+/// lnx is a fast and efficient search engine for performing full-text search, user-facing search
+/// and analytics.
+pub struct Args {
+    #[arg(long, env = "LNX_LOG_LEVEL", default_value = "info")]
+    /// Set the log level of the server.
+    ///
+    /// This can filter on various levels, for example `info,lnx::indexer=warn`
+    /// will display all logs at `info` level severity and above, except for log
+    /// events that appear within the `lnx::indexer` block of operations.
+    log_level: String,
+    #[arg(long, env = "LNX_LOG_JSON")]
+    /// Emit logs in JSON format rather than as plain text.
+    log_json: bool,
+    #[arg(long, env = "LNX_LOG_NO_ANSI")]
+    /// Disable ANSI colour codes being present in the logs.
+    ///
+    /// ANSI codes can cause rendering issues on various observability platforms
+    /// or text file logging due to its escape sequences.
+    log_no_ansi: bool,
+    #[command(subcommand)]
+    command: commands::Commands,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    init_logging(&args)
+        .context("Init logging")?;
+
+    info!("🦀 lnx v{}", env!("CARGO_PKG_VERSION"));
+    info!("Check us out @ {}", env!("CARGO_PKG_HOMEPAGE"));
+    info!("Support @ {}/discord", env!("CARGO_PKG_HOMEPAGE"));
+    info!("Docs @ {}/docs", env!("CARGO_PKG_HOMEPAGE"));
+    
+    args.command.display_startup_message();
+
+
+    Ok(())
+}
